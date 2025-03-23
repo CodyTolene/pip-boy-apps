0722d310bbbdaf8f188827985b6e4606668ef1b8
Cody Tolene <codyatolene@gmail.com>
2025-03-23 10:07:59 -0500

    Add 'type' to app meta. Other polishing.

--- USER/PipPong.js
+++ USER/PipPong.js
@@ -1,8 +1,9 @@
 // =============================================================================
-//  App/Game: Pip-Pong
+//  Name: Pip-Pong
 //  License: CC-BY-NC-4.0
 //  Repository: https://github.com/CodyTolene/pip-apps
 //  Description: A simple pong game for the Pip-Boy 3000 Mk V.
+//  Version: 1.0.0
 // =============================================================================
 
 const SCREEN_WIDTH = g.getWidth();
