2242377bf6f776cdcb695d17abb335cd8b1aadba
Athene Allen <atheneallen93@gmail.com>
2025-04-10 10:56:50 +0100

    add theme picker and custom UI color theming

--- USER_BOOT/ui_color.js
+++ USER_BOOT/ui_color.js
@@ -1,23 +1,66 @@
 // =============================================================================
-//  Name: UI Color - Orange
+//  Name: UI Color - Custom Theme
 //  Link: https://log.robco-industries.org/log/entry016/
-//  Description: Update the Pip-Boy UI color to a custom orange palette.
-//  Version: 1.0.0
+//  Description: Update the Pip-Boy UI color to a custom palette. Custom
+//  palettes can be set with the Theme Picker user app
+//  Version: 1.1.0
 // =============================================================================
 
-for (
-  var pal = [
-      new Uint16Array(16),
-      new Uint16Array(16),
-      new Uint16Array(16),
-      new Uint16Array(16),
-    ],
-    i = 0;
-  i < 16;
-  i++
-)
-  (pal[0][i] = g.toColor(i / 15, i / 30, 0)),
-    (pal[1][i] = g.toColor(i / 30, i / 60, 0)),
-    (pal[2][i] = g.toColor(i / 10, i / 20, 0)),
-    (pal[3][i] = g.toColor(i / 20, i / 40, 0));
-Pip.setPalette(pal);
+function readThemeFile() {
+  const themeSettingsFolder = 'USER/ThemePicker';
+  const themeSettingsFile = 'USER/ThemePicker/theme.json';
+  //try to read directory first, if we error, make the directory and return, nothing to read.
+  try {
+    require('fs').readdirSync(themeSettingsFolder);
+  } catch {
+    require('fs').mkdir(themeSettingsFolder);
+    return null;
+  }
+  try {
+    let fileString = require('fs').readFileSync(themeSettingsFile);
+    log('read ' + fileString + 'from ' + themeSettingsFile);
+    let fileObj = JSON.parse(fileString);
+    return fileObj;
+  } catch {
+    //folder created but no theme file yet, ignore
+    return null;
+  }
+}
+
+let theme = readThemeFile();
+if (theme != null) {
+  for (
+    var pal = [
+        new Uint16Array(16),
+        new Uint16Array(16),
+        new Uint16Array(16),
+        new Uint16Array(16),
+      ],
+      i = 0;
+    i < 16;
+    i++
+  )
+    (pal[0][i] = g.toColor(
+      (theme[0] * i) / 16,
+      (theme[1] * i) / 16,
+      (theme[2] * i) / 16,
+    )),
+      (pal[1][i] = g.toColor(
+        (theme[0] * i) / 16 - (i * 0.2) / 16,
+        (theme[1] * i) / 16 - (i * 0.2) / 16,
+        (theme[2] * i) / 16 - (i * 0.2) / 16,
+      )),
+      (pal[2][i] = g.toColor(
+        (theme[0] * i) / 16 + (i * 0.2) / 16,
+        (theme[1] * i) / 16 + (i * 0.2) / 16,
+        (theme[2] * i) / 16 + (i * 0.2) / 16,
+      )),
+      (pal[3][i] = g.toColor(
+        (theme[0] * i) / 16 - (i * 0.4) / 16,
+        (theme[1] * i) / 16 - (i * 0.4) / 16,
+        (theme[2] * i) / 16 - (i * 0.4) / 16,
+      ));
+  Pip.setPalette(pal);
+}
+delete theme;
+delete readThemeFile;
