1bcfe9fc817a2a70a6962a2ce6c97c656d69f95f
Cody Tolene <codyatolene@gmail.com>
2026-02-13 18:17:42 -0600

    Fix up pointer

--- apps/flappyroach/app.js
+++ apps/flappyroach/app.js
@@ -1,4 +1,4 @@
 // Flappy-Roach Loader
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 log('[Flappy-Roach] Loading app...');
-eval(fs.readFile('USER/FLAPPYROACH/Flappy-Roach.min.js'));
+eval(fs.readFile('USER/FLAPPYROACH/Flappy-Roach.js'));
